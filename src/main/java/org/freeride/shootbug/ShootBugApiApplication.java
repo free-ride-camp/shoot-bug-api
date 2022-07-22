b13bf7f95eb68ef596cb4d89ee13e94f277932ff
zenox <13506553843@163.com>
2022-07-23 01:43:10 +0800

    第一次提交

---
+++ src/main/java/org/freeride/shootbug/ShootBugApiApplication.java
@@ -0,0 +1,13 @@
+package org.freeride.shootbug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShootBugApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShootBugApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/freeride/shootbug/ShootBugApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.freeride.shootbug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShootBugApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
